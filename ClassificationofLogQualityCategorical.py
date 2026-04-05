1728ee6da725f5a63f8b8d335fe99191def29be5
Dimitris Panagiotidis <104321315+panagdim@users.noreply.github.com>
2026-04-05 15:57:47 +0300

    Update ClassificationofLogQualityCategorical.py

--- ClassificationofLogQualityCategorical.py
+++ ClassificationofLogQualityCategorical.py
@@ -2,6 +2,7 @@ import pandas as pd
 import tkinter as tk
 from tkinter import filedialog
 
+# -------------VERSION 1.1------------------------
 # 1. Define classification rules based on height, diameter, and defects (categorical)
 
 def classify_tree(row):
